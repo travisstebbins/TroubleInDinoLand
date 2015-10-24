06e921ac135aea3cb494f2185cf9b40cbca2d696
Travis Stebbins <stebbins.travis@gmail.com>
2015-10-24 05:21:30 -0500

    Implement basic networking/multiplayer functionality

---
+++ Assets/Scripts/NetworkManagerScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManagerScript : MonoBehaviour {
+
+	// public variables
+	public GameObject playerPrefab;
+	public Transform hostSpawnPoint;
+	public Transform clientSpawnPoint;
+
+	// private variables
+	private const string typeName = "Chillennium.NetflixAndChillennium.MultiplayerTest";
+	private const string gameName = "MultiplayerTestGame";
+	private HostData[] hostList;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	private void StartServer () {
+		Network.InitializeServer (2, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (typeName, gameName);
+	}
+
+	void OnServerInitialized () {
+		SpawnPlayer (0);
+	}
+
+	private void RefreshHostList () {
+		MasterServer.RequestHostList (typeName);
+	}
+
+	void OnMasterServerEvent (MasterServerEvent msEvent) {
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList ();
+	}
+
+	private void JoinServer (HostData hostData) {
+		Network.Connect (hostData);
+	}
+
+	void OnConnectedToServer () {
+		SpawnPlayer (1);
+	}
+
+	private void SpawnPlayer (int spawnPointID) {
+		if (spawnPointID == 0) {
+			Network.Instantiate (playerPrefab, hostSpawnPoint.position, Quaternion.identity, 0);
+		} else if (spawnPointID == 1) {
+			Network.Instantiate (playerPrefab, clientSpawnPoint.position, Quaternion.identity, 0);
+		}
+	}
+
+	void OnGUI () {
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
+				StartServer ();
+			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			if (hostList != null) {
+				for (int i = 0; i < hostList.Length; ++i) {
+					if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer (hostList[i]);
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// public variables
+	public float maxSpeed = 10f;
+	public float jumpHeight = 10f;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
+	public LayerMask groundLayerMask;
+	
+	// components
+	private Rigidbody2D rb;
+	//private NetworkView networkView;
+	
+	// private variables
+	private bool isGrounded = false;
+	private bool doubleJump = false;
+	private bool facingRight = false;
+		// for OnSerializeNetworkView
+		private float lastSynchronizationTime = 0f;
+		private float syncDelay = 0f;
+		private float syncTime = 0f;
+		private Vector3 syncStartPosition = Vector3.zero;
+		private Vector3 syncEndPosition = Vector3.zero;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		//networkView = GetComponent<NetworkView> ();
+	}
+	
+	void FixedUpdate () {
+		if (GetComponent<NetworkView>().isMine) {
+			// check if character is grounded
+			isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundLayerMask);
+			if (isGrounded)
+				doubleJump = false;
+		
+			float move = Input.GetAxis ("Horizontal");
+			rb.velocity = new Vector2 ((isGrounded ? move * maxSpeed : move * maxSpeed * 0.8f), rb.velocity.y);
+
+			if (facingRight && move > 0)
+				Flip ();
+			else if (!facingRight && move < 0)
+				Flip ();
+		}
+	}
+	
+	void Update () {
+		if (GetComponent<NetworkView> ().isMine) {
+			if ((isGrounded || !doubleJump) && Input.GetKeyDown (KeyCode.UpArrow)) {
+				if (!isGrounded && !doubleJump)
+					doubleJump = true;
+				rb.velocity = new Vector2 (rb.velocity.x, Mathf.Sqrt (2f * jumpHeight * -Physics2D.gravity.y));			
+			}
+		} else {
+			SyncedMovement();
+		}
+	}
+
+	private void SyncedMovement () {
+		syncTime += Time.deltaTime;
+		rb.position = Vector3.Lerp (syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+		if (stream.isWriting) {
+			syncPosition = rb.position;
+			stream.Serialize (ref syncPosition);
+
+			syncVelocity = rb.velocity;
+			stream.Serialize (ref syncVelocity);
+		} else {
+			stream.Serialize (ref syncPosition);
+			stream.Serialize (ref syncVelocity);
+
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rb.position;
+		}
+	}
+
+	void Flip () {
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+		facingRight = !facingRight;
+	}
+}
